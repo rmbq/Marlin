70ffcaf00ea6e6aefc2beede77bdfe4c35ebbbdc
rmbq <davide.rombola@gmail.com>
2022-12-21 13:27:09 +0100

    cr10smart: update hotend and bed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -668,9 +668,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  23.63
-    #define DEFAULT_Ki   1.96
-    #define DEFAULT_Kd  71.31
+    #define DEFAULT_Kp  20.46
+    #define DEFAULT_Ki   1.95
+    #define DEFAULT_Kd  53.60
   #endif
 #endif
 
@@ -753,9 +753,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 690.34
-  #define DEFAULT_bedKi 111.47
-  #define DEFAULT_bedKd 1068.83
+  #define DEFAULT_bedKp 127.21
+  #define DEFAULT_bedKi  17.30
+  #define DEFAULT_bedKd 623.65
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 
