75555b310d0e2374ccb9d9da39760197820acb04
Davide Rombolà <davide.rombola@gmail.com>
2023-03-02 13:59:31 +0100

    [rmbq] enable LCD_SCREEN_ROTATE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3050,7 +3050,7 @@
 #if DGUS_UI_IS(MKS)
   #define USE_MKS_GREEN_UI
 #elif DGUS_UI_IS(IA_CREALITY)
-  //#define LCD_SCREEN_ROTATE 90 // Portrait Mode or 800x480 displays
+  #define LCD_SCREEN_ROTATE 90 // Portrait Mode or 800x480 displays
 #endif
 
 //
