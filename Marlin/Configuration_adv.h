3b63d097aa079fe5c314407558f8480b3c6155fb
Davide Rombolà <davide.rombola@gmail.com>
2023-05-12 16:46:50 +0200

    [rmbq] update E0_CURRENT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2988,7 +2988,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      700
+    #define E0_CURRENT      900
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.15
     #define E0_CHAIN_POS     -1
