92bd4b0aa7841fab01758033e84607c77ca98bdb
Davide Rombolà <davide.rombola@gmail.com>
2023-08-20 08:39:33 +0200

    [rmbq] update Z_PROBE_OFFSET_RANGE_MIN

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1630,7 +1630,7 @@
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -2
+#define Z_PROBE_OFFSET_RANGE_MIN -4
 #define Z_PROBE_OFFSET_RANGE_MAX 0
 
 // Enable the M48 repeatability test to test probe accuracy
