a3abf96e4ce232bfc45dbf88b1f9197758ff190f
rmbq <davide.rombola@gmail.com>
2025-02-20 11:19:56 +0100

    Merge remote-tracking branch 'markniu/bugfix-2.1.x' into bdsensor2

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -2715,7 +2715,7 @@ void prepare_line_to_destination() {
           probe.stow();
           return;
         }
-        TERN_(BD_SENSOR, bdl.config_state = BDS_HOMING_Z);
+        TERN_(BD_SENSOR, bdl.prepare_homing());
       }
     #endif
 
@@ -2983,10 +2983,6 @@ void prepare_line_to_destination() {
 
     #endif
 
-    #if ALL(BD_SENSOR, HOMING_Z_WITH_PROBE)
-      if (axis == Z_AXIS) bdl.config_state = BDS_IDLE;
-    #endif
-
     // Put away the Z probe
     if (TERN0(HOMING_Z_WITH_PROBE, axis == Z_AXIS && probe.stow())) return;
 
@@ -3069,8 +3065,7 @@ void set_axis_is_at_home(const AxisEnum axis) {
     if (axis == Z_AXIS) {
       #if HOMING_Z_WITH_PROBE
         #if ENABLED(BD_SENSOR)
-          safe_delay(100);
-          current_position.z = bdl.read();
+          bdl.end_homing();  
         #else
           current_position.z -= probe.offset.z;
         #endif
